93f118159a8e5325913691d99619afd7f6af3e16
jcarklin <jcarklin@gmail.com>
2020-07-26 11:20:48 +0200

    convert GTT xml to object

--- lib/src/service/gttConversion.dart
+++ lib/src/service/gttConversion.dart
@@ -1,9 +1,6 @@
-
-
 import 'package:xml/xml.dart';
 
 class GttConversion {
-
   final TwData _twData = TwData();
 
   GttConversion({String xmlInput}) {
@@ -19,43 +16,88 @@ class GttConversion {
     _twData.version = twDataElement.getElement('Version').innerText;
     final patternElement = twDataElement.getElement('Pattern');
     _twData.pattern = Pattern(
-        name: patternElement.getElement('Name').innerText,
-        type: patternElement.getAttribute('Type'),
+      name: patternElement.getElement('Name').innerText,
+      type: patternElement.getAttribute('Type'),
     );
-    var el = patternElement.getElement('Notes');
-    _twData.pattern.notes = el.descendants.map((e) => e.innerText.trim()).join(' ').trim();
-    el = patternElement.getElement('Cards');
-    Card card;
-    XmlElement holesEl;
-    final cards = el.findElements('Card').map((element) {
-      card = Card(cardHoles: element.getAttribute('Holes'), number: element.getAttribute('Number'));
-      holesEl = element.getElement('Holes');
-      card.holes = Holes(colour: [],count: holesEl.getAttribute('Count'));
-      holesEl.findElements('Colour').forEach((element) {
-        card.holes.colour.add(element.innerText);
-      });
-      return card;
-    }).toList();
-    _twData.pattern.cards = Cards(card: cards, count: el.getAttribute('Count'));
+    _twData.pattern.notes = patternElement
+        .getElement('Notes')
+        .descendants
+        .map((e) => e.innerText.trim())
+        .join(' ')
+        .trim();
+
+    var el = patternElement.getElement('Cards');
+    _twData.pattern.cards = Cards(
+        card: el
+            .findElements('Card')
+            .map((element) => _createCard(element))
+            .toList(),
+        count: el.getAttribute('Count'));
+
+    el = patternElement.getElement('Packs');
+    if (el.getAttribute('Count') != '0') {
+      _twData.pattern.packs = Packs(
+        packs: el.findElements('Pack').map((e) => _createPack(e)).toList(),
+        count: el.getAttribute('Count'),
+      );
+    }
+
+    el = patternElement.getElement('Palette');
+    _twData.pattern.palette = Palette(
+      name: el.getAttribute('Name'),
+      size: el.getAttribute('Size'),
+      colour: el.findElements('Colour').map((e) {
+        return Colour(index: e.getAttribute('Index'), text: e.innerText);
+      }).toList(),
+    );
     print(_twData);
   }
 
+  Card _createCard(XmlElement cardElement) {
+    XmlElement innerEl;
+    var card = Card(
+        cardHoles: cardElement.getAttribute('Holes'),
+        number: cardElement.getAttribute('Number'));
+    innerEl = cardElement.getElement('Holes');
+    card.holes = Holes(colour: [], count: innerEl.getAttribute('Count'));
+    innerEl.findElements('Colour').forEach((element) {
+      card.holes.colour.add(element.innerText);
+    });
+    card.threading = cardElement.getElement('Threading').innerText == 'Z'
+        ? Threading.Z
+        : Threading.S;
+    card.curThread = cardElement.getElement('CurThread').innerText == 'Z'
+        ? Threading.Z
+        : Threading.S;
+    card.curPos = cardElement.getElement('CurThread').innerText;
+    innerEl = cardElement.getElement('CurHoles');
+    card.curHoles = Holes(colour: [], count: innerEl.getAttribute('Count'));
+    innerEl.findElements('Colour').forEach((element) {
+      card.curHoles.colour.add(element.innerText);
+    });
+    return card;
+  }
 
-
-
+  Pack _createPack(XmlElement packElement) {
+    return Pack(
+        name: packElement.getAttribute('name'),
+        comment: packElement.getElement('Comment').innerText,
+        size: packElement.getElement('Size').innerText,
+        cards: packElement.getElement('Cards').innerText,
+    );
+  }
 }
 
-
 class TwData {
+  String source;
+  String version;
+  Pattern pattern;
+
   TwData({
     this.source,
     this.version,
     this.pattern,
   });
-
-  String source;
-  String version;
-  Pattern pattern;
 
   @override
   String toString() {
@@ -64,6 +106,14 @@ class TwData {
 }
 
 class Pattern {
+  String name;
+  String notes;
+  Cards cards;
+  Packs packs;
+  Picks picks;
+  Palette palette;
+  String type;
+
   Pattern({
     this.name,
     this.notes,
@@ -74,17 +124,10 @@ class Pattern {
     this.type,
   });
 
-  String name;
-  String notes;
-  Cards cards;
-  Packs packs;
-  Picks picks;
-  Palette palette;
-  String type;
-
   @override
   String toString() {
-    return 'Pattern{name: $name, notes: $notes, cards: $cards, packs: $packs, picks: $picks, palette: $palette, type: $type}';
+    return 'Pattern{name: $name, notes: $notes, cards: $cards, packs: $packs, '
+        'picks: $picks, palette: $palette, type: $type}';
   }
 }
 
@@ -115,8 +158,8 @@ class Card {
   });
 
   Holes holes;
-  CurThread threading;
-  CurThread curThread;
+  Threading threading;
+  Threading curThread;
   String curPos;
   Holes curHoles;
   String cardHoles;
@@ -124,7 +167,9 @@ class Card {
 
   @override
   String toString() {
-    return 'Card{holes: $holes, threading: $threading, curThread: $curThread, curPos: $curPos, curHoles: $curHoles, cardHoles: $cardHoles, number: $number}';
+    return 'Card{holes: $holes, threading: $threading, curThread: $curThread, '
+        'curPos: $curPos, curHoles: $curHoles, '
+        'cardHoles: $cardHoles, number: $number}';
   }
 }
 
@@ -143,36 +188,65 @@ class Holes {
   }
 }
 
-enum CurThread { Z, S }
-
 class Packs {
+  List<Pack> packs;
+  String count;
+
   Packs({
+    this.packs,
     this.count,
   });
 
-  String count;
+  @override
+  String toString() {
+    return 'Packs{packs: $packs, count: $count}';
+  }
+}
+
+class Pack {
+  String name;
+  String comment;
+  String size;
+  String cards;
+
+  Pack({this.name, this.comment, this.size, this.cards});
+
+  @override
+  String toString() {
+    return 'Pack{name: $name, comment: $comment, size: $size, cards: $cards}';
+  }
 }
 
 class Palette {
+  List<Colour> colour;
+  String name;
+  String size;
+
   Palette({
     this.colour,
     this.name,
     this.size,
   });
 
-  List<Colour> colour;
-  String name;
-  String size;
+  @override
+  String toString() {
+    return 'Palette{colour: $colour, name: $name, size: $size}';
+  }
 }
 
 class Colour {
+  String index;
+  String text;
+
   Colour({
     this.index,
     this.text,
   });
 
-  String index;
-  String text;
+  @override
+  String toString() {
+    return 'Colour{index: $index, text: $text}';
+  }
 }
 
 class Picks {
@@ -222,7 +296,6 @@ class Action {
 }
 
 enum Dir { F, B }
-
 enum Target { CARD }
-
 enum Type { TURN }
+enum Threading { Z, S }
